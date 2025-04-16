bd22546224820ec916bc0290e3e1bd9478dc914d
user-arbuzer <srmal@yandex.ru>
2025-04-16 18:45:07 +0300

    все задачи блока 3

--- 3.1/Slovari/char_frequency(s).py
+++
@@ -1,4 +0,0 @@
-def char_frequency(s):
-    return {char: s.count(char) for char in set(s)}
-
-char_frequency("hello")  # {'h': 1, 'e': 1, 'l': 2, 'o': 1}
--- 3.1/Slovari/merge_dicts(dict1, dict2).py
+++
@@ -1,6 +0,0 @@
-def merge_dicts(dict1, dict2):
-    dict1 = {"a": 1, "b": 2}
-    dict2 = {"b": 3, "c": 4}
-    return dict1.update(dict2)
-
-print(merge_dicts(dict1, dict2))  # {"a": 1, "b": 5, "c": 4}
