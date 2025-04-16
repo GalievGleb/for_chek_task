ebf5d3a6946a4876df91f4c97db49fbeae8710c5
Isakov8 <ibadulla98@gmail.com>
2025-04-16 15:45:21 +0300

    Delete 3.0.5 directory

--- 3.0.5/check_string_length(string, length).py
+++
@@ -1,8 +0,0 @@
-def check_string_length(string, length):
-    if len(string) >= length:
-        print(f'Длина "{string}" достаточная')
-    else:
-        print(f'Строка "{string}" слишком короткая')
-string=input("Введите строку:")
-length= int(input("Введите длину:"))
-check_string_length(string, length)
