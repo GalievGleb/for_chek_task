508d48d0ad162c36adbc23cefafc684e8c810648
user-arbuzer <srmal@yandex.ru>
2025-04-30 23:29:55 +0300

    Delete 3.0 directory

--- 3.0/Cycles/count_vowels(string).py
+++
@@ -1,9 +0,0 @@
-#Задача 3: Подсчёт гласных в строке
-def count_vowels(string):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    count = 0
-    for i in string:
-        if i.lower() in vowels:
-            count += 1
-    return count
-print(f"Количество гласных в строке: {count_vowels("Hello World")}")
--- 3.0/practical exercises/number_sum.py
+++
@@ -1,7 +0,0 @@
-def numbers_sum():
-    n = int(input("Введите число: "))
-    numbers = list(range(1, n+1))
-    print("Числа:", ' '.join(map(str, numbers)))
-    total = sum(numbers)
-    print("Сумма чисел:", total)
-numbers_sum()
